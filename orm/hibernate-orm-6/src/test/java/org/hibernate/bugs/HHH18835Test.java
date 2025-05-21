6ff0efa70beb351f5830b527080080be97c04291
peter1123581321 <peter.bambazek@gmail.com>
2025-05-21 16:11:45 +0200

    HHH-18835

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/HHH18835Test.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/HHH18835Test.java
@@ -17,56 +17,45 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DomainModel(
         annotatedClasses = {
-                // Add your entities here.
-                InsertTest.Person.class,
-                InsertTest.Document.class
+                HHH18835Test.Person.class,
+                HHH18835Test.Document.class
         }
 )
 @ServiceRegistry(
-        // Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
         settings = {
-                // For your own convenience to see generated queries:
                 @Setting(name = AvailableSettings.SHOW_SQL, value = "true"),
                 @Setting(name = AvailableSettings.FORMAT_SQL, value = "true"),
                 // @Setting( name = AvailableSettings.GENERATE_STATISTICS, value = "true" ),
         }
 )
 @SessionFactory
-public class InsertTest {
+public class HHH18835Test {
 
     @Test
     void hhh18835Test(SessionFactoryScope scope) {
 
-
+        // prepare person
         scope.inTransaction(session -> {
             Person person = new Person();
             person.name = "Peter";
             session.persist(person);
         });
 
-		/*
-		with id: SimpleInsertQueryPlan
-		without id: MultiTableInsertQueryPlan
-		 */
+        // insert document
         scope.inTransaction(session -> {
-
-//			Person person = session.createQuery( "select p from Person p", Person.class ).getSingleResult();
-
             session.createMutationQuery(
-//							"insert into Document(id, name,owner) select 3,concat(p.name,'s document'), p from Person p" )
                             "insert into Document(name,owner) select concat(p.name,'s document'), p from Person p")
-//							"insert into Document(name,owner) values ('test',:person)" )
-//					.setParameter( "person", person )
                     .executeUpdate();
-
         });
 
+        // assert person
         scope.inTransaction(session -> {
             Person person = session.createQuery("select p from Person p", Person.class).getSingleResult();
             assertNotNull(person);
             assertEquals("Peter", person.name);
         });
 
+        // assert document
         scope.inTransaction(session -> {
             Document document = session.createQuery("select d from Document d", Document.class).getSingleResult();
             assertNotNull(document);
@@ -85,30 +74,15 @@ public class InsertTest {
     }
 
     @Entity(name = "Document")
-//	@SecondaryTable(name = "test")
     public static class Document {
 
         @Id
         @GeneratedValue
-//		@GeneratedValue(
-//				strategy = GenerationType.TABLE,
-//				generator = "hibernate_sequence"
-//		)
-//		@SequenceGenerator(
-//				name = "hibernate_sequence",
-//				allocationSize = 1
-//		)
         Long id;
 
         String name;
 
-//		@Column(table = "test")
-//		String test;
-
         @ManyToOne
         Person owner;
-
-//		@Version
-//		int version;
     }
 }
