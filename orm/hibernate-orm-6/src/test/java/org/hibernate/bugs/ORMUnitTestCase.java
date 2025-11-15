ecda1edcc6a460889b12016ac001c4375da29573
Peter Bambazek <peter.bambazek@gmail.com>
2025-11-15 18:07:35 +0100

    testcase for a json version bug

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -15,20 +15,27 @@
  */
 package org.hibernate.bugs;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.testing.orm.junit.*;
+import org.hibernate.type.SqlTypes;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @DomainModel(
         annotatedClasses = {
-
+                ORMUnitTestCase.MyPerson.class
         }
 )
 @ServiceRegistry(
         settings = {
                 @Setting(name = AvailableSettings.SHOW_SQL, value = "true"),
-                @Setting(name = AvailableSettings.FORMAT_SQL, value = "true")
+                @Setting(name = AvailableSettings.FORMAT_SQL, value = "false")
         }
 )
 @SessionFactory
@@ -37,8 +44,103 @@ class ORMUnitTestCase {
     @Test
     void hhh123Test(SessionFactoryScope scope) {
 
+        // insert (warum weren hier nach 10 insert noch 10 updates gemacht?)
         scope.inTransaction(session -> {
-            // Do stuff...
+            for (int i = 0; i < 10; i++) {
+                MyPerson person = new MyPerson("Peter" + i);
+                person.myJson = new MyJson();
+                session.persist(person);
+            }
         });
+
+        // assert (warum werden hier nach dem select dann noch 10 updates ausgeführt?)
+        scope.inTransaction(session -> {
+            List<MyPerson> persons = session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+            assertEquals(10, persons.size());
+            for (int i = 0; i < 10; i++) {
+                MyPerson person = persons.get(i);
+                assertEquals(1, person.version);
+            }
+        });
+
+        // process
+
+        // n*(n+1)/2, variante wie im transfer service
+        scope.inTransaction(session -> {
+            List<Long> ids = session.createQuery("select id from MyPerson p", Long.class).getResultList();
+            for (int i = 0; i < 10; i++) {
+                MyPerson myPerson = session.createQuery("select p from MyPerson p where id=:id", MyPerson.class)
+                        .setParameter("id", ids.get(i)).getSingleResult();
+                myPerson.name = "Neu" + i;
+            }
+        });
+
+        // hier werden nach jedem der 11 select anschließend 10 updates ausgeführt
+        /*
+        scope.inTransaction(session -> {
+            List<MyPerson> persons = session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+            for (int i = 0; i < 10; i++) {
+                session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+                MyPerson person = persons.get(i);
+                person.name = "Neu" + i;
+            }
+        });
+         */
+
+        // zuerst 11 selects und anschließend 10 updates -- ist das die richtige variante?
+        /*
+        scope.inTransaction(session -> {
+            List<MyPerson> persons = session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+            for (int i = 0; i < 10; i++) {
+                int personIndex = i;
+                AtomicReference<MyPerson> myPerson = new AtomicReference<>();
+                scope.inTransaction(session2 -> {
+                    session2.setDefaultReadOnly(true);
+                    myPerson.set(session2.createQuery("select p from MyPerson p where id=:id", MyPerson.class)
+                            .setParameter("id", persons.get(personIndex).id)
+                            .getSingleResult());
+                });
+                myPerson.get().name = "Neuer Name";
+            }
+        });
+         */
+
+        // assert
+        scope.inTransaction(session -> {
+            List<MyPerson> persons = session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+            assertEquals(10, persons.size());
+            for (int i = 0; i < 10; i++) {
+                MyPerson person = persons.get(i);
+                assertEquals(1, person.version);
+            }
+        });
+    }
+
+    @Entity(name = "MyPerson")
+    public static class MyPerson {
+
+        @Id
+        @GeneratedValue
+        Long id;
+
+        String name;
+
+        @JdbcTypeCode(SqlTypes.JSON)
+        @Column(name = "my_json")
+        MyJson myJson;
+
+        @Version
+        int version;
+
+        public MyPerson() {
+        }
+
+        public MyPerson(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class MyJson {
+        public String name = "name";
     }
 }
---
+++ orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.bugs;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.testing.orm.junit.*;
+import org.hibernate.type.SqlTypes;
+import org.junit.jupiter.api.Test;
+
+
+@DomainModel(
+        annotatedClasses = {
+                ORMUnitTestCase.MyPerson.class
+        }
+)
+@ServiceRegistry(
+        settings = {
+                @Setting(name = AvailableSettings.SHOW_SQL, value = "true"),
+                @Setting(name = AvailableSettings.FORMAT_SQL, value = "false")
+        }
+)
+@SessionFactory
+class ORMUnitTestCase {
+
+    @Test
+    void savePerson(SessionFactoryScope scope) {
+        scope.inTransaction(session -> {
+            MyPerson person = new MyPerson("name");
+            person.myJson = new MyJson();
+            session.persist(person);
+
+            // Hibernate: insert into MyPerson (my_json,name,version,id) values (?,?,?,?)
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+
+            // note: update not needed
+        });
+    }
+
+    @Test
+    void saveTwoPersons_modifyFirst_oneTransaction(SessionFactoryScope scope) {
+        scope.inTransaction(session -> {
+
+            MyPerson first = new MyPerson("name");
+            first.myJson = new MyJson();
+            session.persist(first);
+
+            MyPerson second = new MyPerson("name");
+            second.myJson = new MyJson();
+            session.persist(second);
+
+            first.name = "changed";
+
+            // Hibernate: insert into MyPerson (my_json,name,version,id) values (?,?,?,?)
+            // Hibernate: insert into MyPerson (my_json,name,version,id) values (?,?,?,?)
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+
+            // note: second update not needed
+        });
+
+    }
+
+    @Test
+    void saveTwoPersons_modifyFirst_threeTransactions(SessionFactoryScope scope) {
+        scope.inTransaction(session -> {
+
+            MyPerson first = new MyPerson("name");
+            first.myJson = new MyJson();
+            session.persist(first);
+
+            MyPerson second = new MyPerson("name");
+            second.myJson = new MyJson();
+            session.persist(second);
+            
+            // Hibernate: insert into MyPerson (my_json,name,version,id) values (?,?,?,?)
+            // Hibernate: insert into MyPerson (my_json,name,version,id) values (?,?,?,?)
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+
+            // note: updates not needed
+        });
+
+        scope.inTransaction(session -> {
+
+            MyPerson myPerson = session
+                    .createQuery("select p from MyPerson p", MyPerson.class)
+                    .getResultList()
+                    .stream().findFirst().get();
+
+            myPerson.name = "changed";
+
+            // Hibernate: select mp1_0.id,mp1_0.my_json,mp1_0.name,mp1_0.version from MyPerson mp1_0
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+
+            // note: second update not needed
+        });
+
+        scope.inTransaction(session -> {
+
+            session.createQuery("select p from MyPerson p", MyPerson.class).getResultList();
+
+            // Hibernate: select mp1_0.id,mp1_0.my_json,mp1_0.name,mp1_0.version from MyPerson mp1_0
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+            // Hibernate: update MyPerson set my_json=?,name=?,version=? where id=? and version=?
+
+            // note: no updates needed
+        });
+    }
+
+    @Entity(name = "MyPerson")
+    public static class MyPerson {
+
+        @Id
+        @GeneratedValue
+        Long id;
+
+        String name;
+
+        @JdbcTypeCode(SqlTypes.JSON)
+        @Column(name = "my_json")
+        MyJson myJson;
+
+        @Version
+        int version;
+
+        public MyPerson() {
+        }
+
+        public MyPerson(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class MyJson {
+
+    }
+}
